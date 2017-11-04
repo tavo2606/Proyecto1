8a6e6b12a6c05fbd41982ffec5c0e70db1c74f0c
Tavo <Tavo@Tavo>
2017-11-03 23:59:24 -0600

    Avance en registrarse

--- src/proyecto1/Principal.java
+++ src/proyecto1/Principal.java
@@ -4,7 +4,7 @@ import Interfaz.*;
 public class Principal {
 
     public static void main(String[] args) {
-        Loggin v = new Loggin();
+        Login v = new Login();
         v.setVisible(true);
     }
 }
